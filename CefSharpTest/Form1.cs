8b431d52a5d58c07ec86cc6225de9d7cbc9a6091
Philip Van Raalte <phil.vanraalte@gmail.com>
2017-05-13 20:24:34 -0400

    Add project files.

---
+++ CefSharpTest/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+using System.IO;
+
+//http://ourcodeworld.com/articles/read/173/how-to-use-cefsharp-chromium-embedded-framework-csharp-in-a-winforms-application
+namespace CefSharpTest
+{
+    public partial class Form1 : Form
+    {
+        public ChromiumWebBrowser chromeBrowser;
+
+        public void InitializeChromium()
+        {
+            CefSettings settings = new CefSettings();
+
+            // Note that if you get an error or a white screen, you may be doing something wrong !
+            // Try to load a local file that you're sure that exists and give the complete path instead to test
+            // for example, replace page with a direct path instead :
+            // String page = @"C:\Users\SDkCarlos\Desktop\afolder\index.html";
+
+            String page = string.Format(@"{0}\html-resources\html\index.html", Application.StartupPath);
+            //String page = @"C:\Users\SDkCarlos\Desktop\artyom-HOMEPAGE\index.html";
+
+            if (!File.Exists(page))
+            {
+                MessageBox.Show("Error The html file doesn't exists : " + page);
+            }
+
+            // Initialize cef with the provided settings
+            Cef.Initialize(settings);
+            // Create a browser component
+            chromeBrowser = new ChromiumWebBrowser(page); //new ChromiumWebBrowser("http://ourcodeworld.com");
+            // Add it to the form and fill it to the form window.
+            this.Controls.Add(chromeBrowser);
+            chromeBrowser.Dock = DockStyle.Fill;
+
+            // Allow the use of local resources in the browser
+            BrowserSettings browserSettings = new BrowserSettings();
+            browserSettings.FileAccessFromFileUrls = CefState.Enabled;
+            browserSettings.UniversalAccessFromFileUrls = CefState.Enabled;
+            chromeBrowser.BrowserSettings = browserSettings;
+        }
+
+        public void openDevTools(object sender, EventArgs e)
+        {
+            chromeBrowser.ShowDevTools();
+        }
+
+        public void runScript(object sender, EventArgs e)
+        {
+            var script = "hello();";
+
+            chromeBrowser.GetMainFrame().ExecuteJavaScriptAsync(script);
+        }
+
+        public void getFromScript(object sender, EventArgs e)
+        {
+            var task = chromeBrowser.EvaluateScriptAsync("getMessage()");
+
+            task.ContinueWith(t =>
+            {
+                if (!t.IsFaulted)
+                {
+                    var response = t.Result;
+                    Object b = response.Success ? (response.Result ?? "null") : response.Message;
+                    try
+                    {
+                        Console.WriteLine((String)b);
+                    }
+                    catch (Exception ex) { }
+                }
+            }, TaskScheduler.FromCurrentSynchronizationContext());
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            // Start the browser after initialize global component
+            InitializeChromium();
+            // Register an object in javascript named "cefCustomObject" with function of the CefCustomObject class :3
+            chromeBrowser.RegisterJsObject("cefCustomObject", new CefCustomObject(chromeBrowser, this));
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Cef.Shutdown();
+        }
+    }
+}
